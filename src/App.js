33ad4f42df8c4f85cdbdfd5661baba11ed9afd09
Armaghan Orakzai <armaghan.orakzai15@gmail.com>
2021-07-12 19:33:48 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useReducer } from "react";
+import "./App.css";
 
-function App() {
+import Header from "./Components/Header";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "addTodo":
+      return {
+        todos: [
+          ...state.todos,
+          { key: Math.random(), title: action.enteredTitle, completed: false },
+        ],
+      };
+
+    case "toggleTodo":
+      return {
+        todos: state.todos.map((text, index) =>
+          index === action.index
+            ? { ...text, completed: !text.completed }
+            : text
+        ),
+        todoCount: state.todoCount,
+      };
+
+    default:
+      return state.todos;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { todos: [] });
+
+  const [enteredTitle, setEnteredTitle] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    dispatch({ type: "addTodo", enteredTitle });
+    setEnteredTitle("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Header />
+      <form onSubmit={formSubmitHandler}>
+        <label htmlFor="title">Title</label>
+        <input
+          value={enteredTitle}
+          id="title"
+          type="text"
+          onChange={titleChangeHandler}
+        />
+      </form>
+
+      {state.todos.map((todo, index) => (
+        <div
+          className="todo"
+          key={todo.key}
+          onClick={() => dispatch({ type: "toggleTodo", index })}
+          style={{ textDecoration: todo.completed ? "line-through" : " " }}
         >
-          Learn React
-        </a>
-      </header>
+          <h2>{todo.title}</h2>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1>
+        <a
+          target="_blank"
+          href="https://reactjs.org/docs/hooks-reference.html#usereducer"
+        >
+          useReducer
+        </a>{" "}
+        in{" "}
+        <a target="_blank" href="https://reactjs.org/">
+          React
+        </a>
+      </h1>
+      <hr />
+    </div>
+  );
+};
+
+export default Header;
